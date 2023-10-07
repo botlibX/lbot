bd66f30bf5afadedf78158b3f5faca479266cd4d
Bart Thate <bthate@dds.nl>
2023-10-07 16:26:04 +0200

    iniital import

---
+++ lbot/modules/__init__.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=W0406,C0413
+# flake8: noqa
+
+
+"modules"
+
+
+import os
+import sys
+
+
+from . import cmd, irc, log, man, mod, rss, sts, tdo, thr
+
+
+def __dir__():
+    return (
+            'cmd',
+            'irc',
+            'log',
+            'man',
+            'mod',
+            'rss',
+            'sts',
+            'tdo',
+            'thr'
+           )
